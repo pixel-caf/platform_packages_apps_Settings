765ad9f229ad5f88435dd7c687d5c0f1a04a0ab5
Offain <fabian.leutenegger@bluewin.ch>
2017-03-13 13:39:25 +0100

    Settings: Define COLOR_PUBLIC
    Define COLOR_PUBLIC because it was missing. Also define COLOR_WARNING to be prepared for the future.

--- src/com/android/settings/deviceinfo/StorageSettings.java
+++ src/com/android/settings/deviceinfo/StorageSettings.java
@@ -75,6 +75,9 @@ public class StorageSettings extends SettingsPreferenceFragment implements Index
     private static final String TAG_VOLUME_UNMOUNTED = "volume_unmounted";
     private static final String TAG_DISK_INIT = "disk_init";
 
+    static final int COLOR_PUBLIC = Color.parseColor("#ff9e9e9e");
+    static final int COLOR_WARNING = Color.parseColor("#fff4511e");
+
     static int getColorPublic(Resources resources) {
         return resources.getColor(R.color.storage_public);
     }
